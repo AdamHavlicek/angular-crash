14cb4197383027271d33bb1b81bb25467b74762d
Adam Havlíček <havlicek.adam23@gmail.com>
2021-07-06 12:41:19 +0200

    fix

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { Post } from 'src/app/model/post.model';
-import { AppState } from 'src/app/store/app.state';
-import { updatePost, UPDATE_POST_START } from '../state/posts.actions';
-import { getPostById } from '../state/posts.selector';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Store } from '@ngrx/store'
+import { Subscription } from 'rxjs'
+import { Post } from 'src/app/model/post.model'
+import { AppState } from 'src/app/store/app.state'
+import { updatePost } from '../state/posts.actions'
+import { getPostById } from '../state/posts.selector'
 
 @Component({
     selector: 'app-edit-post',
     templateUrl: './edit-post.component.html',
-    styleUrls: ['./edit-post.component.sass'],
+    styleUrls: ['./edit-post.component.sass']
 })
 export class EditPostComponent implements OnInit, OnDestroy {
-    post: Post;
-    postForm: FormGroup;
-    postSubscription: Subscription;
+    post: Post
+    postForm: FormGroup
+    postSubscription: Subscription
 
-    constructor(
-        private readonly store: Store<AppState>,
-    ) {}
+    constructor(private readonly store: Store<AppState>) {}
 
     ngOnInit(): void {
         this.createForm()
-        this.postSubscription = this.store.select(getPostById).subscribe((post) => {
-            if (!!!post) {
-                return
-            }
-            this.post = post
-            this.postForm.patchValue({
-                title: post.title,
-                description: post.description
+        this.postSubscription = this.store
+            .select(getPostById)
+            .subscribe((post) => {
+                if (!!!post) {
+                    return
+                }
+                this.post = post
+                this.postForm.patchValue({
+                    title: post.title,
+                    description: post.description
+                })
             })
-        })
         // this.route.paramMap.subscribe((params) => {
         //     const id = params.get('id');
         //     this.store.select(getPostById(id)).subscribe((data) => {
@@ -45,22 +44,22 @@ export class EditPostComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         if (this.postSubscription) {
-            this.postSubscription.unsubscribe();
+            this.postSubscription.unsubscribe()
         }
     }
 
     onSubmit(): void {
         if (!this.postForm.valid) {
-            return;
+            return
         }
 
         const post: Post = {
             ...this.post,
             title: this.postForm.value.title,
-            description: this.postForm.value.description,
-        };
+            description: this.postForm.value.description
+        }
 
-        this.store.dispatch(updatePost({ post }));
+        this.store.dispatch(updatePost({ post }))
         // this.router.navigate(['posts']);
     }
 
@@ -68,12 +67,12 @@ export class EditPostComponent implements OnInit, OnDestroy {
         this.postForm = new FormGroup({
             title: new FormControl(null, [
                 Validators.required,
-                Validators.minLength(6),
+                Validators.minLength(6)
             ]),
             description: new FormControl(null, [
                 Validators.required,
-                Validators.minLength(10),
-            ]),
-        });
+                Validators.minLength(10)
+            ])
+        })
     }
 }
